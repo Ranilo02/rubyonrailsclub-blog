d28244b2df891d0b33819efe8decf28e70a78ec4
Ciro Ranilo Batista Cabral <ciro.cabral@etipi.pi.gov.br>
2024-09-30 11:31:55 -0300

    separando a area publica e administrativa do site

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles
+  get "welcome/index"
+  namespace :administrate do
+    resources :articles
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
